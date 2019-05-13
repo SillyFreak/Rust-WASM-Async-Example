3552a20d36365a0bbd3c5ec3b28ee587e85527ce
SillyFreak <sillyfreak.ck@gmail.com>
2019-05-13 12:46:49 +0200

    don't implement sleep() backwards, even though it shows off the Rust/JS interop

--- crate/src/sleep.rs
+++ crate/src/sleep.rs
@@ -3,23 +3,16 @@ use wasm_bindgen::prelude::*;
 pub async fn sleep(millis: i32) -> Result<(), JsValue> {
     use crate::compat::PromiseExt;
 
-    let promise = js_sys::Promise::new(&mut move |resolve, _| {
-        let window = web_sys::window().expect("should have a Window");
-        window.set_timeout_with_callback_and_timeout_and_arguments_0(
-            &resolve, millis
-        ).expect("don't expect error on setTimeout()");
-    });
-
-    promise.to_future().await?;
+    sleep_js(millis).to_future().await?;
     Ok(())
 }
 
 #[wasm_bindgen(js_name = sleep)]
 pub fn sleep_js(millis: i32) -> js_sys::Promise {
-    use crate::compat::FutureExt;
-
-    Box::pin(async move {
-        sleep(millis).await?;
-        Ok(JsValue::UNDEFINED)
-    }).to_promise()
+    js_sys::Promise::new(&mut move |resolve, _| {
+        let window = web_sys::window().expect("should have a Window");
+        window.set_timeout_with_callback_and_timeout_and_arguments_0(
+            &resolve, millis
+        ).expect("don't expect error on setTimeout()");
+    })
 }
