0a59878cf7350dff5b0bc2a7550254266e279bd4
SillyFreak <sillyfreak.ck@gmail.com>
2019-05-13 12:41:31 +0200

    organize imports

--- crate/src/compat.rs
+++ crate/src/compat.rs
@@ -1,6 +1,16 @@
 use std::future::Future;
+use futures::compat::{
+    Compat,
+    Compat01As03,
+};
 use js_sys::Promise;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::{
+    JsFuture as JsFuture01,
+    future_to_promise as future01_to_promise,
+};
+
+type JsFuture = Compat01As03<JsFuture01>;
 
 pub trait FutureExt {
     fn to_promise(self) -> Promise;
@@ -9,15 +19,12 @@ pub trait FutureExt {
 impl<F> FutureExt for F
         where F: Future<Output=Result<JsValue, JsValue>> + 'static {
     fn to_promise(self) -> Promise {
-        use wasm_bindgen_futures::future_to_promise;
-        use futures::compat::Compat;
-
         // pin 0.3
         let future = Box::pin(self);
         // 0.3 to 0.1
         let future = Compat::new(future);
         // 0.1 to promise
-        future_to_promise(future)
+        future01_to_promise(future)
     }
 }
 
@@ -25,10 +32,6 @@ pub trait PromiseExt<F>
         where F: Future<Output=Result<JsValue, JsValue>> {
     fn to_future(self) -> F;
 }
-
-use wasm_bindgen_futures::JsFuture as JsFuture01;
-use futures::compat::Compat01As03;
-type JsFuture = Compat01As03<JsFuture01>;
 
 impl PromiseExt<JsFuture> for Promise {
     fn to_future(self) -> JsFuture {
